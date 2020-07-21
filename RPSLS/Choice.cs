a4d71cdcfe668243a699720c24e20eec99858318
MILAN STOJANOVIC <milanstoj94@gmail.com>
2020-07-21 07:59:34 -0500

    final

--- RPSLS/Choice.cs
+++ RPSLS/Choice.cs
@@ -9,67 +9,11 @@ namespace RPSLS
     class Choice
     {
         public List<string> choices;
-        // public List<string> choiceImage;
+        
 
         public Choice()
         {
             choices = new List<string>() { "Rock", "Paper", "Scissors", "Lizard", "Spock" };
-            /*choiceGraphic = new List<string>()
-            {
-                @"
-            
-                _______
-            ---'   ____)
-                  (_____)
-           ROCK   (_____)
-                  (____)
-            ---.__(___)
-            ",
-                @"
-                 _______
-            ---'    ____)____
-                       ______)
-           PAPER      _______)
-                     _______)
-            ---.__________)
-            ",
-                @"
-                _______
-            ---'   ____)____
-                      ______)
-          SCISSORS__________)
-                  (____)
-            ---.__(___)
-            ",
-
-                @"
-                 __  ,
-                 (' \ \
-                  \ \\/ 
-                '\/\ \\
-                    \ \\/\,
-                     \ \\
-           LIZARD    /\ \\
-                     \ `\\\
-                     '   `\\
-                           \\    ,
-                            `---' 
-            ",
-                @"
-                        ,                          
-               __  _.-'` `'-.                      
-              /||\'._ __{}_(  
-              ||||  |'--.__\ 
-              |  L.(   ^_\^                        
-              \ .-' |   _ |                        
-       SPOCK  | |   )\___/                         
-              |  \-'`:._]                          
-              \__/;      '-.                       
-              |   |o     __ \                      
-              |   |o     )( |                      
-              |   |o     \/ \                      
-                                  "
-            };*/
         }
     }
 }
